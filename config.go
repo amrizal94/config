8276ca076dd89a2f1c74e33e12c4a45a6991ab40
amrizal94 <arimrizal94@gmail.com>
2022-12-28 08:58:44 +0700

    add module mysql

--- config.go
+++ config.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
 
@@ -13,28 +14,28 @@ type Config struct {
 	DBUser string
 	DBPass string
 	DBHost string
-	DBName string
 	DBPort int
+	DBName string
 }
 
-func ReadConfig(namefile string) *Config {
-	err := godotenv.Load(namefile)
+func ReadConfig() *Config {
+	err := godotenv.Load("local.env")
 	if err != nil {
 		fmt.Println("Error saat baca env", err.Error())
 		return nil
 	}
+
 	var res Config
 	res.DBUser = os.Getenv("DBUSER")
 	res.DBPass = os.Getenv("DBPASS")
 	res.DBHost = os.Getenv("DBHOST")
-	res.DBName = os.Getenv("DBNAME")
 	readData := os.Getenv("DBPORT")
 	res.DBPort, err = strconv.Atoi(readData)
 	if err != nil {
 		fmt.Println("Error saat convert", err.Error())
 		return nil
 	}
-
+	res.DBName = os.Getenv("DBNAME")
 	return &res
 }
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/amrizal94/config/v3
 
 go 1.19
 
-require github.com/joho/godotenv v1.4.0
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/joho/godotenv v1.4.0
+)
